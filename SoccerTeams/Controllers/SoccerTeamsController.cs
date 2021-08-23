b13646a186ae7b10aa0470ee41bd64875660ec5c
okanulkr <okanulkr@hotmail.com>
2021-08-24 01:23:04 +0300

    Initial commit

---
+++ SoccerTeams/Controllers/SoccerTeamsController.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SoccerTeams.Controllers
+{
+    [ApiController]
+    [Route("api/v1/teams")]
+    public class SoccerTeamsController : ControllerBase
+    {
+        [HttpGet("{count}")]
+        public IEnumerable<Team> Get(int count)
+        {
+            return new Faker<Team>()
+                .RuleFor(x => x.Name, (f, u) => "FC " + f.Name.LastName())
+                .RuleFor(x => x.Logo, (f, u) => f.Image.LoremFlickrUrl(width: 150, height: 150, keywords: "football"))
+                .Generate(count);
+        }
+    }
+}
---
+++ SoccerTeams/Team.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SoccerTeams
+{
+    public class Team
+    {
+
+        public string Name { get; set; }
+
+        public string Logo { get; set; }
+    }
+}
